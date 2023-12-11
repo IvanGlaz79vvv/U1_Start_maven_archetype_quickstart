bb65855c14ed63698a0964f63cb6ba6de4d54529
IvanTest-31 <ivangl79@gmail.com>
2023-12-11 07:10:31 +0300

    Initial commit

---
+++ src/main/spring_introduction/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+//        Dog dog = new Dog();
+//        Pet pet = new Dog();
+        Pet pet = new Cat();
+        pet.say();
+    }
+}
